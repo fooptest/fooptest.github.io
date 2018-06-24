516b43d3d2c6947934388c0009aa00df9c0b2a64
Sis C <campbellka@byui.edu>
2018-06-23 18:28:53 -0600

    q
    q

--- currentdate.js
+++ currentdate.js
@@ -1,4 +1,4 @@
-var d = new Date();
+function setDate(){var d = new Date();
 var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-document.getElementById("currentdate").innerHTML = day[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
+document.getElementById("currentdate").innerHTML = day[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();}
