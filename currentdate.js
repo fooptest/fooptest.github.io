40b4979e1ba6965dc5a0aaba4cf5b5adad8a296f
Sis C <campbellka@byui.edu>
2018-06-23 20:50:35 -0600

    eee

--- currentdate.js
+++ currentdate.js
@@ -4,4 +4,5 @@ async function setDate(){
 var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 document.getElementById("currentdate").innerHTML = day[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
+    return v;
                   }
