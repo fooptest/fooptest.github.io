6aa0af08f57d63faddd4c1175962119a5d7d4714
Sis C <campbellka@byui.edu>
2018-06-23 20:25:54 -0600

    try
    try

--- currentdate.js
+++ currentdate.js
@@ -1,4 +1,6 @@
-function setDate(){var d = new Date();
+function setDate() {
+    await include();
+    var d = new Date();
 var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 document.getElementById("currentdate").innerHTML = day[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
