09e25331bdd56178c75dc04dc3a21182bf156730
Sis C <campbellka@byui.edu>
2018-06-23 20:53:13 -0600

    ee

--- include.js
+++ include.js
@@ -25,4 +25,13 @@ function include() {
 
     }
 }
+async function setDate(){
+    let v=await include();
+    var d = new Date();
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("currentdate").innerHTML = day[d.getDay()] + ", " + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear();
+    return v;
+                  }
+
  // initial function call
